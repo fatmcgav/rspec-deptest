67fd2a06dac66e104e859d3cefb9627c37d67e4f
Gavin Williams <fatmcgav@gmail.com>
2014-12-04 11:23:25 +0000

    Revert work-around for PUP-3336, prior to testing PR #237

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,13 @@ require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(Dir.pwd, 'spec/fixtures'))
 
-# Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
-#
-if Puppet.version < "4.0.0"
-  Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
-    $LOAD_PATH << lib_dir
-  end
-end
+# # Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
+# #
+# if Puppet.version < "4.0.0"
+#   Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
+#     $LOAD_PATH << lib_dir
+#   end
+# end
 
 RSpec.configure do |c|
   c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
